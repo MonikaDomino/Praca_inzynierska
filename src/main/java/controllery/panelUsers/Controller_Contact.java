1044ca31ecc68482dbfe1b78721967245f0756a1
Monika Domino <47738234+md5687@users.noreply.github.com>
2020-12-01 00:15:08 +0100

    formularz kontaktowy

---
+++ src/main/java/controllery/panelUsers/Controller_Contact.java
@@ -0,0 +1,24 @@
+package controllery.panelUsers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller_Contact {
+
+    @FXML
+    private TextField emailtxt;
+
+    @FXML
+    private TextField temattxt;
+
+    @FXML
+    private TextArea wiadomosctxt;
+
+    @FXML
+    void sendEmail(ActionEvent event) {
+
+    }
+
+}
