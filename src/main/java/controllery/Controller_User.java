d123c372ac6928be2092a3030f5203a797fcf0bd
Monika Domino <47738234+md5687@users.noreply.github.com>
2020-12-01 00:14:33 +0100

    formularz kontaktowy

--- src/main/java/controllery/Controller_User.java
+++ src/main/java/controllery/Controller_User.java
@@ -2,11 +2,22 @@ package controllery;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Controller_User extends openFXMl {
+
+
+    @FXML
+    private BorderPane borderPane;
 
 
     @FXML
@@ -17,6 +28,43 @@ public class Controller_User extends openFXMl {
 
     }
 
+    @FXML
+    void backToStart(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToAnalysis(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToCompanyData(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToContact(MouseEvent event) {
+
+        loadPage("/fxml/panelUser_type/panelContact");
+
+    }
+
+    @FXML
+    void goToPersonalData(MouseEvent event) {
+
+    }
+
+    private void loadPage (String page) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource(page+".fxml"));
+        } catch (IOException e) {
+            Logger.getLogger(Controller_User.class.getName()).log(Level.SEVERE, null, e);
+        }
+            borderPane.setCenter(root);
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
