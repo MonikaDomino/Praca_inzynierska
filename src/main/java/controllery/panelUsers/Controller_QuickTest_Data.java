771fe73e6ba08aaba8e920e5493dd982f68ce066
Monika Domino <47738234+md5687@users.noreply.github.com>
2020-12-06 22:53:18 +0100

    formularze - Wprowadź dane - Test i Analiza dyskryminacyjna

--- src/main/java/controllery/panelUsers/Controller_QuickTest_Data.java
+++ src/main/java/controllery/panelUsers/Controller_QuickTest_Data.java
@@ -2,18 +2,49 @@ package controllery.panelUsers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
-import java.io.IOException;
-
 public class Controller_QuickTest_Data {
-
     @FXML
     private Pane paneData;
 
     @FXML
-    void readNewFXML(ActionEvent event) throws IOException {
+    private TextField net_profit;
+
+    @FXML
+    private TextField total_assets;
+
+    @FXML
+    private TextField equity_capital;
+
+    @FXML
+    private TextField amortization;
+
+    @FXML
+    private TextField operation_profit;
+
+    @FXML
+    private TextField total_Sales;
+
+    @FXML
+    private TextField credits;
+
+    @FXML
+    private TextField yeartxt;
+
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clearForm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doTest(ActionEvent event) {
 
     }
 
